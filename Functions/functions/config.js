45f340186aaf1fc3ec2ee905c15d9944e6176816
hyma2 <Hyma200@gmail.com>
2021-01-30 13:44:51 -0500

    Package installations and set up features

--- Functions/functions/config.js
+++ Functions/functions/config.js
@@ -6,6 +6,6 @@ const firebaseConfig = {
     messagingSenderId: "982051622055",
     appId: "1:982051622055:web:7cae90250deb9416891f82",
     measurementId: "G-2W3LX503TM"
-};
+  };
 
-module.exports = config;
+  module.exports = firebaseConfig;
--- Functions/functions/index.js
+++ Functions/functions/index.js
@@ -1,9 +1,20 @@
+const config = require('./config');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const firebase = require('firebase');
+const {v4: uuid} = require("uuid");
+admin.initializeApp();
+const db = admin.firestore();
+const app = require('express')();
+const cors = require('cors');
+app.use(cors());
+firebase.initializeApp(config);
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+   functions.logger.info("Hello logs!", {structuredData: true});
+   response.send("Hello from Firebase!");
+ });
